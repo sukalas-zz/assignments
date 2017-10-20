f042690b39cd8ebe6c1579fb52f76651faa1cf0b
unknown <litridis@gmail.com>
2017-10-20 15:07:30 +0200

    sepperated ajax call

---
+++ ajax.js
@@ -0,0 +1,15 @@
+function ajax(url){
+	let xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function() {
+	    if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
+	    	respArr = xmlHttp.responseText;
+	    	console.log(respArr)
+	    }
+	}
+	xmlHttp.addEventListener('load', (respArr) => {
+		processList(respArr);
+	});
+
+	xmlHttp.open('GET', url, true); //Asynchronous
+	xmlHttp.send( null );
+}
